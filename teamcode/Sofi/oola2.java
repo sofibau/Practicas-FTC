8463a9db12de245c70871c40aacb293bedd69e16
sofibau <59583358+sofibau@users.noreply.github.com>
2020-07-15 17:44:25 -0500

    porf

---
+++ teamcode/Sofi/oola2.java
@@ -0,0 +1,10 @@
+package teamcode.Sofi;
+
+public class oola2 {
+
+    public static void main(String[] args) {
+        System.out.println("ola");
+        System.out.println("ola");
+        System.out.println("ola");
+}
+}
